47dfff48b8b5bddf498f2fe9424a831bd18bac83
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-06-11 16:38:20 +0300

    feat: добавить деактивацию кнопок слайдера

--- scripts/index.js
+++ scripts/index.js
@@ -10,20 +10,22 @@ const body = document.querySelector('.page'),
 // console.log(sliderItems);
 
 const sliderCounter = 0;
-//обработчики
 
-// sliderItems.forEach((item, idx, arr) => {
-//   if (item.classList.contains('slider__item_is-visible')) {
-//     item.classList.remove('slider__item_is-visible');
-//     sliderItems[idx+1].classList.add('slider__item_is-visible');
-//     console.log('removed');
-//     debugger;
-//   }
-// });
+//обработчики
 
 rightArrowBtn.addEventListener('click', () => {
   slideNext(sliderItems);
+  handleBtnState(rightArrowBtn);
+  activateBtn(leftArrowBtn);
 });
+
+leftArrowBtn.addEventListener('click', () => {
+  slidePrevious(sliderItems);
+  handleBtnState(leftArrowBtn);
+  activateBtn(rightArrowBtn);
+});
+
+//функции
 
 function slideNext(arr) {
 
@@ -32,14 +34,33 @@ function slideNext(arr) {
 
       removeItemClass(item);
       if (idx < (arr.length -1)) {
-        addItemClass(arr[idx + 1])
-        return true;
-      } else if (idx == (arr.length -1)) {
-        // addItemClass(arr[0]);
-        addItemClass(arr[0]);
+        addItemClass(arr[idx + 1]);
         return true;
       }
+      // } else if (idx == (arr.length -1)) {
+      //   // addItemClass(arr[0]);
+      //   addItemClass(arr[0]);
+      //   return true;
+      // }
 
+    }
+  });
+}
+
+function slidePrevious(arr) {
+
+  arr.some((item, idx) => {
+    if (item.classList.contains('slider__item_is-visible')) {
+
+      removeItemClass(item);
+      if (idx < arr.length && idx > 0) {
+        addItemClass(arr[idx - 1]);
+        return true;
+      } else if (idx == 0) {
+        // addItemClass(arr[0]);
+        addItemClass(arr[arr.length -1]);
+        return true;
+      }
 
     }
   });
@@ -51,4 +72,28 @@ function removeItemClass(item) {
 
 function addItemClass(item) {
   item.classList.add('slider__item_is-visible');
+}
+
+function handleBtnState(btn) {
+  sliderItems.forEach((item, idx, arr) => {
+    if (item.classList.contains('slider__item_is-visible')) {
+      if (idx === 0 || idx === (arr.length -1)) {
+        disableBtn(btn);
+      // } else {
+      //   activateBtn(btn);
+      }
+    } else {
+      activateBtn(btn);
+    }
+  });
+}
+
+function disableBtn(btn) {
+  btn.classList.add('slider__button_inactive');
+  btn.setAttribute('disabled', 'disabled');
+}
+
+function activateBtn(btn) {
+  btn.classList.remove('slider__button_inactive');
+  btn.removeAttribute('disabled');
 }
