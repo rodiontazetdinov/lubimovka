800887ee699cb6187a8c7f8b0784fa8ce219255d
@rodiontazetdinov <rodiontazetdinov@gmail.com>
2022-06-16 02:35:22 +0300

    feat: поправить баги в вёрстке, и переписать функцию первой позиции

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,10 @@ const body = document.querySelector('.page'),
       slider = body.querySelector('.slider'),
       leftArrowBtn = slider.querySelector('#left-arrow'),
       rightArrowBtn = slider.querySelector('#right-arrow'),
-      sliderList = slider.querySelector('.slider__list'),
-      sliderItems = Array.from(sliderList.querySelectorAll('.slider__item'));
+      sliderList = slider.querySelector('.slider__list');
 
 const slide = document.querySelector('#slide');
+
 
 //функции
 
@@ -15,33 +15,106 @@ function createSlide(author, text) {
   const element = slide.content.querySelector('.slider__item').cloneNode(true);
   element.querySelector('.slider__signature').textContent = author;
   element.querySelector('.slider__text').textContent = text;
-  element.classList.add('slider__item_is-visible');
 
   return element;
 }
 
 function fillSlideList() {
-    sliderList.innerHTML = '';
-    fillWindow(sliderConfig.firsSlidePosition);
-    sliderConfig.slidesWindow.forEach(item => {
-      const element = createSlide(sliderConfig.slides[item].author, sliderConfig.slides[item].text);
-
-      sliderList.append(element);
-    });
+  const sliderItems = Array.from(sliderList.querySelectorAll('.slider__item'));
+  deactivateSlides(sliderItems);
+  activateSlides(sliderItems);
+  console.log(testConfig());
 }
 
-function fillWindow(firsSlidePosition) { // сюда вместо нуля добавить позишн
-
-  if ((firsSlidePosition + sliderConfig.slidesCount) < sliderConfig.slides.length) {
-    sliderConfig.slidesWindow = [];
-    for (let i = firsSlidePosition; i < (firsSlidePosition + sliderConfig.slidesCount); i++) {
-      sliderConfig.slidesWindow.push(i);
+function deactivateSlides(sliderItems) {
+  sliderItems.forEach((slide, idx) => {
+    if (slide.classList.contains('slider__item_is-visible')) {
+      slide.classList.remove('slider__item_is-visible');
+      console.log(`deactivated ${idx}`);
     }
-  } else {
-    sliderConfig.firsSlidePosition -= sliderConfig.slidesCount;
-    checkArrowBtns();
+  });
+}
+
+function activateSlides(sliderItems) {
+  sliderItems.forEach((slide, idx) => {
+    if (sliderConfig.slidesWindow.indexOf(idx) > -1) {
+      slide.classList.add('slider__item_is-visible');
+      console.log(`activated ${idx}`);
+    }
+  });
+}
+
+function handleBtnState () {
+  const firstSlide = 0;
+  const lastSlide = sliderConfig.lastSlidePosition;
+  sliderConfig.slidesWindow.forEach((item) => {
+    if (item == firstSlide) {
+      disableBtn(leftArrowBtn);
+      activateBtn(rightArrowBtn);
+    } else if (item == lastSlide) {
+      disableBtn(rightArrowBtn);
+      activateBtn(leftArrowBtn);
+    }
+  });
+}
+
+function testConfig() {
+  console.log(`
+  ---------------------------
+  окно
+  ${sliderConfig.slidesWindow}
+  количество слайдов
+  ${sliderConfig.slidesCount}
+  первый слайд
+  ${sliderConfig.firstSlidePosition}
+  ---------------------------
+  `);
+}
+
+
+
+function fillStartSlideList() {
+  sliderConfig.slides.forEach((item, idx) => {
+    const element = createSlide(item.author,item.text);
+    if (idx < sliderConfig.slidesCount) {
+      element.classList.add('slider__item_is-visible');
+    }
+    sliderList.append(element);
+  });
+}
+
+function fillWindow(firstSlidePosition) {
+
+  console.log(firstSlidePosition, 'первая позиция передали в fillWindow');
+  //((sliderConfig.slidesWindow.indexOf(sliderConfig.lastSlidePosition) > -1) && ((sliderConfig.lastSlidePosition + 1) >= 0))
+
+  // if (sliderConfig.slidesWindow.indexOf(sliderConfig.lastSlidePosition) > -1) {
+  //   firstSlidePosition = (sliderConfig.lastSlidePosition + 1) - sliderConfig.slidesCount; //ЗАТЫК ВОТ ТУТ. Когда присутствует последняя карточка. Какое условие нужно поставить, чтобы ограничить срабатывание.
+  //   console.log(firstSlidePosition, 'если последний слайд есть в окне, то первая позиция будет последняя минус кол-во слайдов');
+ // } else
+  if (firstSlidePosition - sliderConfig.slidesCount < 0) {
+    firstSlidePosition = 0;
+    console.log(firstSlidePosition, 'если последней позиции в окне нет, и первая позиция минус кол-во слайдов < 0, то первая позиция будет 0');
   }
 
+  sliderConfig.slidesWindow = [];
+  for (let i = firstSlidePosition; i < (firstSlidePosition + sliderConfig.slidesCount); i++) {
+    console.log(firstSlidePosition, ' for позиция начала передачи в окно');
+    sliderConfig.slidesWindow.push(i);
+    console.log(i, 'for то что передается в окно');
+  }
+
+  handleBtnState();
+  console.log('fillWindow');
+}
+
+function disableBtn(btn) {
+  btn.classList.add('slider__button_inactive');
+  btn.setAttribute('disabled', 'disabled');
+}
+function activateBtn(btn) {
+  btn.classList.remove('slider__button_inactive');
+  btn.removeAttribute('disabled');
 }
 
 function countSlides() {
@@ -49,54 +122,76 @@ function countSlides() {
 }
 
 function slideNext () {
-  if (!rightArrowBtn.hasAttribute('disabled')) {
-    sliderConfig.firsSlidePosition += sliderConfig.slidesCount;
-    console.log(sliderConfig.firsSlidePosition, 'firsSlidePosition');
-    console.log(sliderConfig.slidesWindow, 'slidesWindow');
-    console.log(sliderConfig.slidesCount, 'slidesCount');
-    console.log(sliderConfig.slidesCount, '----------------------------');
+  setFirstSlidePosition('back');
+    if ((sliderConfig.firstSlidePosition + sliderConfig.slidesCount) <= 5) { //если в диапазоне до 5 включительно
+      sliderConfig.firstSlidePosition += sliderConfig.slidesCount;
+    } else if (sliderConfig.slidesWindow.indexOf(sliderConfig.lastSlidePosition) > -1) {
+      sliderConfig.firstSlidePosition = (sliderConfig.lastSlidePosition + 1) - sliderConfig.slidesCount; //ЗАТЫК ВОТ ТУТ. Когда присутствует последняя карточка. Какое условие нужно поставить, чтобы ограничить срабатывание.
+      console.log(sliderConfig.firstSlidePosition, 'если последний слайд есть в окне, то первая позиция будет последняя минус кол-во слайдов');
+    }
+    //sliderConfig.firstSlidePosition += sliderConfig.slidesCount;
 
-  }
+    fillWindow(sliderConfig.firstSlidePosition);
+    fillSlideList();
+
+
+  //}
 }
 
 function slidePrevious () {
-  if (!leftArrowBtn.hasAttribute('disabled')) {
-    sliderConfig.firsSlidePosition -= sliderConfig.slidesCount;
-    console.log(sliderConfig.firsSlidePosition);
-  }
+  setFirstSlidePosition('forward');
+    if ((sliderConfig.firstSlidePosition - sliderConfig.slidesCount) >= 0) {
+      console.log('if');
+      sliderConfig.firstSlidePosition -= sliderConfig.slidesCount;
+    } else if ((sliderConfig.firstSlidePosition - sliderConfig.slidesCount) < 0) {
+      console.log('else if');
+      sliderConfig.firstSlidePosition = 0;
+    }
+
+    //console.log('fillWindow');
+    fillWindow(sliderConfig.firstSlidePosition);
+    //console.log('fillSlideList');
+    fillSlideList();
+    //fillSlideList();
 
 }
 
-function checkArrowBtns () {
-  if ((sliderConfig.firsSlidePosition + sliderConfig.slidesCount) > sliderConfig.slides.length) {
-    rightArrowBtn.setAttribute('disabled', 'disabled');
-    rightArrowBtn.classList.add('slider__button_inactive');
-  } else {
-    if (rightArrowBtn.hasAttribute('disabled')) {
-      rightArrowBtn.removeAttribute('disabled');
-      rightArrowBtn.classList.remove('slider__button_inactive');
-    }
-  }
-  if ((sliderConfig.firsSlidePosition - sliderConfig.slidesCount) < 0) {
-    leftArrowBtn.setAttribute('disabled', 'disabled');
-    leftArrowBtn.classList.add('slider__button_inactive');
-  } else {
-    if (leftArrowBtn.hasAttribute('disabled')) {
-      leftArrowBtn.removeAttribute('disabled');
-      leftArrowBtn.classList.remove('slider__button_inactive');
-    }
-  }
+function updateSlider() {
+  setFirstSlidePosition('update');
 }
 
-// тут создаётся начальное кол-во слайдов
+function setFirstSlidePosition (scenario) {
+  // switch (scenario) {
+  //   case 'forward':
+  //     if (checkForward()) {
+
+  //     } else {
+  //       sliderConfig.firstSlidePosition += sliderConfig.slides;
+  //     }
+  //     break;
+  //   case 'back':
+  //     alert( 'В точку!' );
+  //     break;
+  //   case 'update':
+  //     alert( 'Перебор' );
+  //     break;
+  //   default:
+  //     alert( "Нет таких значений" );
+  // }
+}
+
+function checkForward () {
+  return sliderConfig.firstSlidePosition + sliderConfig.slides;
+}
+
+function checkBack () {
+  return sliderConfig.firstSlidePosition - sliderConfig.slides;
+}
+
 let slidesCount = (Math.floor(window.innerWidth / 420) < 4) ? Math.floor(window.innerWidth / 420) : 3;//   let result = условие ? значение1 : значение2;
-//let slidesWindow = []; // сюда положим индексы от нуля до размера окна
-//slidesWindow.push() // надо придумать как положить туда индексы
-
 
 const sliderConfig = {
   slidesCount: slidesCount,
-  firsSlidePosition: 0,
   slides: [
     {
       author: 'Наталья Зайцева',
@@ -111,158 +206,60 @@ const sliderConfig = {
       text: 'Мне данный формат дал возможность самой выбирать, как двигается персонаж, что на нем надето, какую машину он водит, и что за плакат висит в подвале церкви. Это было приятно, ведь я как будто сама поучаствовала в спектакле'
     },
     {
-      author: 'Наталья Зайцева',
-      text: 'Что-то похожее на эффект от мультфильмов типа «Сауз Парк» или про коня Боджэка возникает — я думаю, и пьеса написана с этой интонацией американских взрослых мультсериалов. И как хорошо все это с куклой-носочком. Так все чисто сделано!'
+      author: 'Леонид Якубович',
+      text: 'Те, кто играл, но в финал не вышел, вы получаете мои эксклюзивные кружечки. Таких нет в магазинах, только у меня. А вы, финалист, завидуйте!'
     },
     {
-      author: 'Дина Годер',
-      text: 'Для самой этой истории формат читки работает отличным ироническим отстранением'
+      author: 'Армин Арлерт',
+      text: 'Ты никогда не перевернёшь ход вещей, если не готов пожертвовать всем'
     },
     {
-      author: 'Дарья Морозова',
-      text: 'Мне данный формат дал возможность самой выбирать, как двигается персонаж, что на нем надето, какую машину он водит, и что за плакат висит в подвале церкви. Это было приятно, ведь я как будто сама поучаствовала в спектакле'
+      author: 'Иван Карамазов',
+      text: 'Я спрашивал себя много раз: есть ли в мире такое отчаяние, чтобы победило во мне эту исступленную и неприличную может быть жажду жизни, и решил, что, кажется, нет такого'
     }
   ],
- slidesWindow: [] //добавить состояние окна, чтобы слайдер не забывал какие именно слайды были отрисованы
+  firstSlidePosition: 0,
+  lastSlidePosition: 5,
+  slidesWindow: []
 };
 
 //обработчики
 
 window.addEventListener('resize',(evt) => {
+  updateSlider();
+  // if (window.innerWidth < 910 ) {
+  //   sliderConfig.slidesCount = 1;
+  //   // countSlides();
+  //   fillWindow(sliderConfig.firstSlidePosition);
+  //   fillSlideList();
 
-  if (window.innerWidth < 910 ) {
-    sliderConfig.slidesCount = 1;
+  // }
+  // if (window.innerWidth >= 910 && window.innerWidth <= 1329) {
+  //   sliderConfig.slidesCount = 2;
+  //   // countSlides();
+  //   fillWindow(sliderConfig.firstSlidePosition);
+  //   fillSlideList();
+  // }
+
+  // if (window.innerWidth >=1330) {
+    //sliderConfig.slidesCount = 3;
+    countSlides();
+    fillWindow(sliderConfig.firstSlidePosition);
     fillSlideList();
-
-  }
-  if (window.innerWidth >= 910 && window.innerWidth <= 1329) {
-    sliderConfig.slidesCount = 2;
-    fillSlideList();
-  }
-
-  if (window.innerWidth >=1330) {
-    sliderConfig.slidesCount = 3;
-
-    fillSlideList();
-  }
+  //}
 });
 
 rightArrowBtn.addEventListener('click', () => {
   slideNext();
-  checkArrowBtns();
 });
 
 leftArrowBtn.addEventListener('click', () => {
   slidePrevious();
-  checkArrowBtns();
 });
 
-
-fillSlideList();
-
-
-//обработчики
+//работа кода
 
 
+fillWindow(sliderConfig.firstSlidePosition);
 
-// function checkLeftArrowBtn () {
-//   if ((sliderConfig.firsSlidePosition - sliderConfig.slidesCount) < 0) {
-//     leftArrowBtn.setAttribute('disabled', 'disabled');
-//     leftArrowBtn.classList.add('slider__button_inactive');
-//   } else {
-//     if (leftArrowBtn.hasAttribute('disabled')) {
-//       leftArrowBtn.removeAttribute('disabled');
-//       leftArrowBtn.classList.remove('slider__button_inactive');
-//     }
-//   }
-//   if ((sliderConfig.firsSlidePosition + sliderConfig.slidesCount) > sliderConfig.slides.length) {
-//     rightArrowBtn.setAttribute('disabled', 'disabled');
-//     rightArrowBtn.classList.add('slider__button_inactive');
-//   } else {
-//     if (rightArrowBtn.hasAttribute('disabled')) {
-//       rightArrowBtn.removeAttribute('disabled');
-//       rightArrowBtn.classList.remove('slider__button_inactive');
-//     }
-//   }
-// }
-
-
-
-// leftArrowBtn.addEventListener('click', () => {
-//   slidePrevious(sliderItems);
-//   handleBtnState(leftArrowBtn);
-//   activateBtn(rightArrowBtn);
-// });
-
-//функции
-
-// function slideNext(arr) {
-
-//   arr.some((item, idx) => {
-//     if (item.classList.contains('slider__item_is-visible')) {
-
-//       removeItemClass(item);
-//       if (idx < (arr.length -1)) {
-//         addItemClass(arr[idx + 1]);
-//         return true;
-//       }
-//       // } else if (idx == (arr.length -1)) {
-//       //   // addItemClass(arr[0]);
-//       //   addItemClass(arr[0]);
-//       //   return true;
-//       // }
-
-//     }
-//   });
-// }
-
-// function slidePrevious(arr) {
-
-//   arr.some((item, idx) => {
-//     if (item.classList.contains('slider__item_is-visible')) {
-
-//       removeItemClass(item);
-//       if (idx < arr.length && idx > 0) {
-//         addItemClass(arr[idx - 1]);
-//         return true;
-//       } else if (idx == 0) {
-//         // addItemClass(arr[0]);
-//         addItemClass(arr[arr.length -1]);
-//         return true;
-//       }
-
-//     }
-//   });
-// }
-
-// function removeItemClass(item) {
-//   item.classList.remove('slider__item_is-visible');
-// }
-
-// function addItemClass(item) {
-//   item.classList.add('slider__item_is-visible');
-// }
-
-// function handleBtnState(btn) {
-//   sliderItems.forEach((item, idx, arr) => {
-//     if (item.classList.contains('slider__item_is-visible')) {
-//       if (idx === 0 || idx === (arr.length -1)) {
-//         disableBtn(btn);
-//       // } else {
-//       //   activateBtn(btn);
-//       }
-//     } else {
-//       activateBtn(btn);
-//     }
-//   });
-// }
-
-// function disableBtn(btn) {
-//   btn.classList.add('slider__button_inactive');
-//   btn.setAttribute('disabled', 'disabled');
-// }
-
-// function activateBtn(btn) {
-//   btn.classList.remove('slider__button_inactive');
-//   btn.removeAttribute('disabled');
-// }
+fillStartSlideList();
